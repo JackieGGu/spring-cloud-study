e1c87e053b16efc885a7af5f5fc50a49a7e31787
顾杰 <gujie@daluyundun.com.cn>
2021-07-09 18:10:19 +0800

    修改消费者服务向注册中心进行注册

--- consumer-service/src/main/java/cn/jackiegu/spring/cloud/consumer/service/impl/CatServiceImpl.java
+++ consumer-service/src/main/java/cn/jackiegu/spring/cloud/consumer/service/impl/CatServiceImpl.java
@@ -3,11 +3,14 @@ package cn.jackiegu.spring.cloud.consumer.service.impl;
 import cn.jackiegu.spring.cloud.consumer.model.CatDTO;
 import cn.jackiegu.spring.cloud.consumer.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.security.SecureRandom;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,11 +25,28 @@ public class CatServiceImpl implements CatService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Override
     public CatDTO get() {
+        CatDTO result;
         SecureRandom random = new SecureRandom();
         Map<String, Object> params = new HashMap<>();
         params.put("randomId", random.nextInt(1000));
-        return restTemplate.getForObject("http://127.0.0.1:10021/producer/cat/generate?id={randomId}", CatDTO.class, params);
+
+        // 使用硬编码方式
+        // result = restTemplate.getForObject("http://127.0.0.1:10021/producer/cat/generate?id={randomId}", CatDTO.class, params);
+
+        // 使用注册中心方式
+        List<ServiceInstance> instances = discoveryClient.getInstances("producer-service");
+        ServiceInstance producerServiceInstance = instances.get(0);
+        String url = "http://" +
+            producerServiceInstance.getHost() +
+            ":" +
+            producerServiceInstance.getPort() +
+            "/producer/cat/generate?id={randomId}";
+        result = restTemplate.getForObject(url, CatDTO.class, params);
+        return result;
     }
 }
