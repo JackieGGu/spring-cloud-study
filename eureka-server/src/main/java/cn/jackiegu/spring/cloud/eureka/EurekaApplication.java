1c4b94548e1c006f7e74ed7df3a836635531e0b1
顾杰 <gujie@daluyundun.com.cn>
2021-07-09 17:23:19 +0800

    新增Eureka服务注册中心

---
+++ eureka-server/src/main/java/cn/jackiegu/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.jackiegu.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka 注册服务中心
+ *
+ * @author JackieGu
+ * @date 2021/7/9
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
