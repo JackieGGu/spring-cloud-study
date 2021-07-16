64e4041806a9a2f6b2fe51cfcdd480e98056addd
顾杰 <gujie@daluyundun.com.cn>
2021-07-16 10:44:55 +0800

    打印日志

--- producer-service/src/main/java/cn/jackiegu/spring/cloud/producer/service/impl/CatServiceImpl.java
+++ producer-service/src/main/java/cn/jackiegu/spring/cloud/producer/service/impl/CatServiceImpl.java
@@ -2,6 +2,8 @@ package cn.jackiegu.spring.cloud.producer.service.impl;
 
 import cn.jackiegu.spring.cloud.producer.model.CatDTO;
 import cn.jackiegu.spring.cloud.producer.service.CatService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class CatServiceImpl implements CatService {
 
+    Logger logger = LoggerFactory.getLogger(CatServiceImpl.class);
+
     @Value("${server.port}")
     private String port;
 
     @Override
     public CatDTO generate(Integer id) {
+        logger.info("Generate Cat entity, id: {}", id);
         if (id == 0) {
             try {
                 Thread.sleep(5 * 1000);
