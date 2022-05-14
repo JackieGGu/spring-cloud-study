df46b619a4ff2dc61ec4eac7fdcfd3ea000a369f
JackieGu-Book <jackieggu@gmail.com>
2022-05-14 12:58:06 +0800

    Eureka Client: add dependency, update hello web api

--- eureka-client/src/main/java/cn/jackiegu/spring/cloud/eureka/client/controller/EurekaClientController.java
+++ eureka-client/src/main/java/cn/jackiegu/spring/cloud/eureka/client/controller/EurekaClientController.java
@@ -1,6 +1,9 @@
 package cn.jackiegu.spring.cloud.eureka.client.controller;
 
+import cn.hutool.json.JSONUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +20,18 @@ import java.util.Map;
 @RequestMapping
 public class EurekaClientController {
 
-    @GetMapping("hello")
-    public String hello() {
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("hello/{name}")
+    public String hello(@PathVariable("name") String name) {
         Map<String, Object> result = new HashMap<>();
         result.put("code", 200);
-        result.put("success", true);
-        return result.toString();
+        result.put("msg", "成功");
+        Map<String, Object> data = new HashMap<>();
+        data.put("port", port);
+        data.put("name", name);
+        result.put("data", data);
+        return JSONUtil.toJsonStr(result);
     }
 }
